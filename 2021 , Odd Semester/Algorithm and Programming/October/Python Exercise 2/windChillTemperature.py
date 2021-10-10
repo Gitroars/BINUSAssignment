be19ca93b53f2c174f7d36ce8914b2536d1175fb
Gitroars <501sterror@gmail.com>
2021-10-10 09:46:01 +0700

    Update windChillTemperature.py

--- 2021 , Odd Semester/Algorithm and Programming/October/Python Exercise 2/windChillTemperature.py
+++ 2021 , Odd Semester/Algorithm and Programming/October/Python Exercise 2/windChillTemperature.py
@@ -10,5 +10,6 @@ while not(windSpeed>=2):
     print("Speed must be greater than or equal to 2")
     temperature = eval(input("Please re-enter the temperature in Fahrenheit: "))
 
-Twc = 35.74 + (0.6215*temperature) - (35.75*(windSpeed**0.16)) + (0.4275*(temperature**0.16))
-print(f"The wind chill index is {(round(Twc,3))}")
+Twc = 35.74 + 0.6215*temperature - 35.75*windSpeed**0.16 + 0.4275*temperature*windSpeed**0.16
+Twc = '{0:,.3f}'.format(Twc)
+print(f"The wind chill index is {Twc}")
