5df902872f55aa83cbbbd4731043e736af1a8ba4
Gitroars <501sterror@gmail.com>
2021-10-12 16:52:17 +0700

    Update rampAngle.py
    Change round method to format

--- 2021 , Odd Semester/Algorithm and Programming/October/Python Exercise 2/rampAngle.py
+++ 2021 , Odd Semester/Algorithm and Programming/October/Python Exercise 2/rampAngle.py
@@ -7,4 +7,6 @@ g = 9.8
 sinX = force/(mass*g)
 x = math.asin(sinX)
 x = math.degrees(x)
-print(f"The angle of the ramp is {round(x,1)}")
+x = '{0:,.1f}'.format(x)
+
+print(f"The angle of the ramp is {x}")
