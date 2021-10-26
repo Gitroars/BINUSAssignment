3b6876229abcb4533efb4c70848bd9843e72aa3b
Gitroars <501sterror@gmail.com>
2021-10-26 14:00:37 +0700

    Finish

--- 2021 , Odd Semester/Algorithm and Programming/October/Functional Programming Exercise/ConvertToDays.py
+++ 2021 , Odd Semester/Algorithm and Programming/October/Functional Programming Exercise/ConvertToDays.py
@@ -3,13 +3,10 @@ def convertToDays():
     minutes = int(input("Enter number of minutes:"))
     seconds = int(input("Enter number of seconds:"))
 
+    def getDays(h,m,s):
+        days = round((h / 24 + m / 1440 + s / 86400),4)
+        return days
+    newDays = getDays(hours,minutes,seconds)
+    print("Total of days is: ",newDays)
 
-    def getDays():
-        days = hours/24 + minutes/1440 + seconds/86400
-        return round(days,4)
-    getDays()
-
-
-
-
-
+convertToDays()
